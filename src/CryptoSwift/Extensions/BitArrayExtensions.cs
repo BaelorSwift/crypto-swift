48c88a2f7fb3f7797089a231ac4ec896913db45c
Alex Forbes-Reed <alexreed@outlook.com>
2016-01-13 22:14:42 +0000

    created extension method for converting a bitarray to a bytearray

--- src/CryptoSwift/Extensions/BitArrayExtensions.cs
+++ src/CryptoSwift/Extensions/BitArrayExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace CryptoSwift.Extensions
 {
@@ -14,6 +15,22 @@ namespace CryptoSwift.Extensions
 					value += Convert.ToInt16(Math.Pow(2, i));
 
 			return value;
+		}
+
+		public static byte[] ToByteArray(this BitArray bitArray)
+		{
+			var bytes = new List<byte>();
+			for (var i = 0; i < bitArray.Length; i += 8)
+			{
+				byte b = 0x00;
+				for (var y = 0; y < 8; y++)
+					if (bitArray[i + y])
+						b |= (byte)(1 << y);
+
+				bytes.Add(b);
+			}
+
+			return bytes.ToArray();
 		}
 	}
 }
