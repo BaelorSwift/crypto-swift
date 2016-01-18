014abf28f5185a552ad573e847b6a153d1c1b3af
Alex Forbes-Reed <alexreed@outlook.com>
2016-01-18 13:23:31 +0000

    fingerprint model docs

--- src/CryptoSwift/Models/Fingerprint.cs
+++ src/CryptoSwift/Models/Fingerprint.cs
@@ -5,15 +5,27 @@ namespace CryptoSwift.Models
 {
 	public class Fingerprint
 	{
+		/// <summary>
+		/// The slug of the song the fingerprint is generated from.
+		/// </summary>
 		[JsonProperty("song_slug")]
 		public string SongSlug { get; set; }
 
+		/// <summary>
+		/// The lyric of the song.
+		/// </summary>
 		[JsonProperty("lyric")]
 		public string Lyric { get; set; }
 
+		/// <summary>
+		/// Where in the song the lyric starts.
+		/// </summary>
 		[JsonProperty("time_code_start")]
 		public TimeSpan TimeCodeStart { get; set; }
 
+		/// <summary>
+		/// Where in the song the lyric ends.
+		/// </summary>
 		[JsonProperty("time_code_end")]
 		public TimeSpan TimeCodeEnd { get; set; }
 	}
