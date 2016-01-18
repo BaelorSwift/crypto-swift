eea739b8d600b7e879cdd7103b9104c10f19573c
Alex Forbes-Reed <alexreed@outlook.com>
2016-01-18 12:39:45 +0000

    updates to try and fix xunit tests on appveyor

--- test/CryptoSwift.Tests/CryptographyManagerTests.cs
+++ test/CryptoSwift.Tests/CryptographyManagerTests.cs
@@ -1,25 +1,30 @@
 ﻿using System;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace CryptoSwift.Tests
 {
 	public class CryptographyManagerTests
 	{
-		private readonly CryptographyManager _cryptoManagaer;
+		//private readonly CryptographyManager _cryptoManagaer;
 
 		public CryptographyManagerTests()
 		{
-			var apiKey = Environment.GetEnvironmentVariable("baelor-test-apikey");
-			var fingerprints = FingerprintManager.GenerateFingerprint(apiKey).Result;
-			_cryptoManagaer = new CryptographyManager(new FingerprintManager(fingerprints));
+		//	var apiKey = Environment.GetEnvironmentVariable("baelor-test-apikey");
+		//	var fingerprints = FingerprintManager.GenerateFingerprint(apiKey).Result;
+		//	_cryptoManagaer = new CryptographyManager(new FingerprintManager(fingerprints));
 		}
 
 		[Fact]
-		public void TestKeyGeneration()
+		public async Task TestKeyGeneration()
 		{
-			var keyA = _cryptoManagaer.GenerateNewKey();
-			var keyB = _cryptoManagaer.GenerateNewKey();
+			var apiKey = Environment.GetEnvironmentVariable("baelor-test-apikey");
+			var fingerprints = await FingerprintManager.GenerateFingerprint(apiKey);
+			var cryptoManagaer = new CryptographyManager(new FingerprintManager(fingerprints));
+
+			var keyA = cryptoManagaer.GenerateNewKey();
+			var keyB = cryptoManagaer.GenerateNewKey();
 
 			Assert.False(keyA == keyB);
 			Assert.True(keyA.Length * 8 == CryptographyManager.KeyBitSize);
@@ -27,10 +32,14 @@ namespace CryptoSwift.Tests
 		}
 
 		[Fact]
-		public void TestIvGeneration()
+		public async Task TestIvGeneration()
 		{
-			var ivA = _cryptoManagaer.GenerateNewIv();
-			var ivB = _cryptoManagaer.GenerateNewIv();
+			var apiKey = Environment.GetEnvironmentVariable("baelor-test-apikey");
+			var fingerprints = await FingerprintManager.GenerateFingerprint(apiKey);
+			var cryptoManagaer = new CryptographyManager(new FingerprintManager(fingerprints));
+
+			var ivA = cryptoManagaer.GenerateNewIv();
+			var ivB = cryptoManagaer.GenerateNewIv();
 
 			Assert.False(ivA == ivB);
 			Assert.True(ivA.Length * 8 == CryptographyManager.BlockBitSize);
@@ -41,18 +50,22 @@ namespace CryptoSwift.Tests
 		[InlineData("1")]
 		[InlineData("11")]
 		[InlineData("Sample string to test encryption and decryption")]
-		public void TestEncryptionAndDecryption(string data)
+		public async Task TestEncryptionAndDecryption(string data)
 		{
-			var key = _cryptoManagaer.GenerateNewKey();
-			var iv = _cryptoManagaer.GenerateNewIv();
+			var apiKey = Environment.GetEnvironmentVariable("baelor-test-apikey");
+			var fingerprints = await FingerprintManager.GenerateFingerprint(apiKey);
+			var cryptoManagaer = new CryptographyManager(new FingerprintManager(fingerprints));
 
-			var encryptedDataFingerprints = _cryptoManagaer.Encrypt(Encoding.ASCII.GetBytes(data), key, iv);
+			var key = cryptoManagaer.GenerateNewKey();
+			var iv = cryptoManagaer.GenerateNewIv();
+
+			var encryptedDataFingerprints = cryptoManagaer.Encrypt(Encoding.ASCII.GetBytes(data), key, iv);
 			var formattedEncryptedFingerprints = "";
 			foreach (var fingerprint in encryptedDataFingerprints)
 				formattedEncryptedFingerprints += fingerprint.Lyric + "\n";
 			formattedEncryptedFingerprints = formattedEncryptedFingerprints.TrimEnd('\n');
 
-			var decryptedData = _cryptoManagaer.Decrypt(formattedEncryptedFingerprints, key, iv);
+			var decryptedData = cryptoManagaer.Decrypt(formattedEncryptedFingerprints, key, iv);
 			var decryptedPlainText = Encoding.ASCII.GetString(decryptedData);
 
 			Assert.True(data == decryptedPlainText);
